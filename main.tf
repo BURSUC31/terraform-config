8fe51a08d56119a1fe5cafd75e3f2133efbf0033
Dimi <tomuleseidimitrie@gmail.com>
2024-04-29 16:38:47 +0300

    formmat

--- main.tf
+++ main.tf
@@ -3,16 +3,10 @@ provider "aws" {
 }
 
 
-variable "vpc_cidr_block" {
-
-}
-variable "subnet_cidr_block" {
-
-}
-variable "avail_zone" {
-}
-variable "env_prefix" {
-}
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "avail_zone" {}
+variable "env_prefix" {}
 
 
 resource "aws_vpc" "myapp-vpc" {
