6e61cb0315959f4880a12955a769c1987dae8171
Dimi <tomuleseidimitrie@gmail.com>
2024-05-02 16:01:56 +0300

    add provisioning throught files

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ variable "env_prefix" {}
 variable "my_ip" {}
 variable "instance_type" {}
 variable "public_key_location" {}
-
+variable "private_key_location" {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -91,14 +91,14 @@ data "aws_ami" "latest-amazon-linux-image" {
 }
 
 output "ec2_public_ip" {
-  value = aws_instance.myapp-server.public_ip
+  value = aws_instance.myapp-server2.public_ip
 }
 resource "aws_key_pair" "ssh-key" {
   key_name   = "server-key-1"
   public_key = file(var.public_key_location)
 }
 
-resource "aws_instance" "myapp-server" {
+resource "aws_instance" "myapp-server2" {
   ami           = data.aws_ami.latest-amazon-linux-image.id
   instance_type = var.instance_type
 
@@ -109,7 +109,25 @@ resource "aws_instance" "myapp-server" {
   associate_public_ip_address = true
   key_name                    = aws_key_pair.ssh-key.key_name
 
-  user_data = file("entry-script.sh")
+  # user_data = file("entry-script.sh")
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.private_key_location)
+    password    = "orice"
+
+  }
+
+  provisioner "file" {
+    source      = "entry-script.sh"
+    destination = "/home/ec2-user/entry-script-on-ec2.sh"
+  }
+  provisioner "remote-exec" {
+    script = file("entry-script-on-ec2.sh")
+
+  }
 
   tags = {
     Name = "${var.env_prefix}-server"
